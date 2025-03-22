bed55c7d220dc318164af20ca2bde569987b65b4
Fiona Stanley <fionakathrynstanley@gmail.com>
2025-03-22 17:45:12 +0000

    Setup mantine ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,50 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import "@mantine/core/styles.css";
+import image1 from "./assets/art/Aquaria.png";
+import image2 from "./assets/art/Blind Spots.png";
+import image3 from "./assets/art/Ecstasy of St Theresa .png";
+import image4 from "./assets/art/Dirty-Dancing.png";
+import image5 from "./assets/art/Donnie Darko.png";
+import image6 from "./assets/art/Eye-Bruised.png";
+import image7 from "./assets/art/Geometric-Hand.png";
+import image8 from "./assets/art/Pulp Fiction.png";
+import image9 from "./assets/art/Sergei Polunin.png";
+import image10 from "./assets/art/Venus.png";
+import image11 from "./assets/art/Geometric-Face.png";
+import image12 from "./assets/art/Sam Morris.png";
+
+import { Container, Grid, MantineProvider } from "@mantine/core";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const images = [
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10,
+    image11,
+    image12,
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <MantineProvider>
+      <Container my="md">
+        <Grid>
+          {images.map((image, index) => {
+            return (
+              <Grid.Col span={{ base: 12, xs: 4 }} key={index}>
+                <img src={image} width={250} />
+              </Grid.Col>
+            );
+          })}
+        </Grid>
+      </Container>
+    </MantineProvider>
   );
 }
 
